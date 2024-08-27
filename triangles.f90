3b1204c1f1686a670b304afc0b10f74a590b023d
jonscales <jon.scales@msutexas.edu>
2024-08-27 16:18:17 -0500

    triangle first try

---
+++ triangles.f90
@@ -0,0 +1,36 @@
+! Jon Scales
+! Fortran program to determine if 3 length segments will generate a triangle
+
+
+program triangles
+    ! All variables will be declared - good practice
+    implicit none 
+    ! Declarations
+    ! String declaration
+    character(len = 15) :: name
+    ! decimal declaration (C++ float or double)
+    real :: num1, num2, num3 
+
+    ! unformatted I/O
+    write (*, *)
+    write (*, *) 'Jon Scales'
+    write (*, *) 'Fortran Triangle Test Program'
+    write (*, *) 'Fall 2024 '
+    write (*, *)
+    write (*, *) 'This program will determine if 3 lengths can be arranged to generat a triangle'
+    write (*, *)
+    write (*, *) 'Please enter your name: '
+    read *, name
+    write (*, *) 'Please enter three length values (separate values with a space) '
+    read *, num1, num2, num3
+    write (*, *) 'Hello ', name
+    if (num1 > (num2 + num3)) then
+      write (*, *) 'These lengths can not be formed into a triangle'
+    else if (num2 > (num1 + num3)) then
+      write (*, *) 'These lengths can not be formed into a triangle'
+    else if (num3 > (num1 + num2)) then
+      write (*, *) 'These lengths can not be formed into a triangle'
+    else
+      write (*, *) 'These lengths could be formed into a triangle'
+    end if 
+  end program simple
