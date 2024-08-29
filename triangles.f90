013c0883797b6f8ca2c4d092a2d4d792bb5c997a
Jon Scales <100714163+jonscales@users.noreply.github.com>
2024-08-29 13:45:48 -0500

    Update triangles.f90

--- triangles.f90
+++ triangles.f90
@@ -7,28 +7,46 @@ program triangles
   
     character(len = 15) :: name
 
-    real :: num1, num2, num3 
+    real :: num1, num2, num3, roundNum1, roundNum2, roundNum3 
+    character(len = 1 ) :: more
 
-  
+    more = 'Y'
     write (*, *)
     write (*, *) 'Jon Scales'
     write (*, *) 'Fortran Triangle Test Program'
     write (*, *) 'Fall 2024 '
     write (*, *)
-    write (*, *) 'This program will determine if 3 lengths can be arranged to generat a triangle'
+    write (*, *) 'This program will determine if 3 lengths can be arranged to generate a triangle'
     write (*, *)
     write (*, *) 'Please enter your name: '
     read *, name
-    write (*, *) 'Please enter three length values (separate values with a space) '
-    read *, num1, num2, num3
-    write (*, *) 'Hello ', name
-    if (num1 > (num2 + num3)) then
-      write (*, *) 'These lengths can not be formed into a triangle'
-    else if (num2 > (num1 + num3)) then
-      write (*, *) 'These lengths can not be formed into a triangle'
-    else if (num3 > (num1 + num2)) then
-      write (*, *) 'These lengths can not be formed into a triangle'
-    else
-      write (*, *) 'These lengths could be formed into a triangle'
-    end if 
+    do while (more == 'Y' .OR. more =='y')
+        write (*, *) 'Please enter three length values (separate values with a space) '
+        read *, num1, num2, num3
+        roundNum1 = NINT(num1 * 10.0) / 10.0
+        roundNum2 = NINT(num2 * 10.0) / 10.0
+        roundNum3 = NINT(num3 * 10.0) / 10.0
+        ! write (*, *) 'Hello ', name
+        if (roundNum1 > (roundNum2 + roundNum3)) then
+          write (*, *) "I'm sorry ", name, ' these lengths can not be formed into a triangle'
+        else if (roundNum2 > (roundNum1 + roundNum3)) then
+          write (*, *) "I'm sorry ", name, ' these lengths can not be formed into a triangle'
+        else if (roundNum3 > (roundNum1 + roundNum2)) then
+          write (*, *) 'These lengths CAN NOT be formed into a triangle'
+        else
+          write (*, *) 'Good news ', name, '! These lengths CAN BE formed into a triangle'
+        end if
+        write (*, *) 'Would you like to test another set of values? (Y or N)'
+        read *, more
+    end do    
+    
+    write (*, *) 'Thanks for using my triangles program! Goodbye'
+    
+    
   end program triangles
+  
+  
+  
+  
+  
+  
